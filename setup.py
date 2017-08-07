c048a459321a5085832196619662bc81a8536dc9
James Cameron <quozl@laptop.org>
2017-08-07 17:47:39 +1000

    remove deprecated bundle_name argument

--- setup.py
+++ setup.py
@@ -17,4 +17,4 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from sugar.activity import bundlebuilder
-bundlebuilder.start('JigsawPuzzle')
+bundlebuilder.start()
